a3acb3b50827cfe26377313b4514a14e114db7c9
RASHWIN SP <rashwinrashwin1@gmail.com>
2026-09-25 12:25:16 +0530

    Time: 2 ms (91.62%) | Memory: 48 MB (6.97%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,17 +1,17 @@
-class Solution {
-    public void moveZeroes(int[] nums) {
-        int index = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                nums[index] = nums[i];
-                index++;
+class Solution 
+{
+    public void moveZeroes(int[] nums)
+    {
+        int left = 0;
+        for (int right = 0; right < nums.length; right++) 
+        {
+            if (nums[right] != 0) 
+            {
+                int temp = nums[right];
+                nums[right] = nums[left];
+                nums[left] = temp;
+                left++;
             }
-        }
-
-        while (index < nums.length) {
-            nums[index] = 0;
-            index++;
-        }
+        }        
     }
 }
